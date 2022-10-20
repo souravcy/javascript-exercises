dd5d0cb5c109c2bd54404e006ce63dcbe5352047
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-20 14:40:31 +0530

    Exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let cel=(temp-32)*5/9;
+  cel=Math.round(cel * 10) / 10
+  return cel;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let cel=(temp*9/5)+32;
+  cel=Math.round(cel * 10) / 10
+  return cel;
 };
 
 // Do not edit below this line
