4028368786ce9bdb9201203bf77c7b0d8618cf78
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-20 14:26:26 +0530

    Exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if((start<0 || typeof(start)!="number") || (end<0 || typeof(end)!="number"))
+        return ("ERROR");
+    if(start>end){
+        let c=start;
+        start=end;
+        end=c;
+    }
+    let sum=0;
+    for(let i=start;i<=end;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
