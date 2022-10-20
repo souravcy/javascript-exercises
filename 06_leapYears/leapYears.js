b5c293f16f7e6d29e5bcee9086f8169157bcfad0
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-20 14:32:55 +0530

    Exercise 6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if((years%4==0 && years%100!=0 )|| years%400==0)
+        return true;
+    return false;
 };
 
 // Do not edit below this line
