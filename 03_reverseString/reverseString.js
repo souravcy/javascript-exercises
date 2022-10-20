a8fa8853af9cf06903708b6f456a85816291357b
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-20 12:34:34 +0530

    Exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    word=Array.from(word);
+    let newWord="";
+    for(let i=word.length-1;i>=0;i--){
+        newWord=newWord.concat(word[i]);
+    }
+    return(newWord);
 };
 
 // Do not edit below this line
