1ad5575777ff042a5a7472e9dbfeec7ee0004797
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-20 12:27:07 +0530

    Exercise one and two done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let newWord="";
+    if(num<0)
+        return("ERROR");
+    for(let i=0;i<num;i++){
+        newWord=newWord.concat(word);
+    }
+    return (newWord);
 };
 
 // Do not edit below this line
