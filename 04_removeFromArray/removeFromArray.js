315baad3e010f72d8f147314fcfffda664fd7563
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-20 14:18:43 +0530

    Exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let arr=arguments[0];
+    let brr=Array.from(arguments);
+    for(let i=0;i<arr.length;i++){
+        for(let j=1;j<brr.length;j++){
+            if(brr[j]==arr[i] && typeof(arr[i])==typeof(brr[j])){
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
